84028afe5815620326a390e4408311695279e85a
engrawais <awais.cns@gmail.com>
2015-09-06 19:24:22 +0500

    aja

--- WMS/Models/Model1.Designer.cs
+++ WMS/Models/Model1.Designer.cs
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 ﻿// Default code generation is disabled for model 'E:\Projects\NHA\NHAGIT\WMS\Models\Model1.edmx'. 
-=======
-﻿// Default code generation is disabled for model 'C:\NHA\NHAGITbackup\WMS\Models\Model1.edmx'. 
->>>>>>> f2386b6df7c17d9c6a97b4400f2810bc70fb9fc2
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
